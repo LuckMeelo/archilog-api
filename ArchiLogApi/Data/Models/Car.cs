d34a4432ba51afe454452ea5f2ad2b5c185ccfe7
LuckMeelo <charmeel.vodouhe@epitech.eu>
2025-01-26 19:30:29 +0100

    chores: init api and library

---
+++ ArchiLogApi/Data/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using ApiClassLibrary.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArchiLogApi.Data.Models
+{
+    public class Car : BaseModel
+    {
+        [MaxLength(150)]
+        [Required]
+        public string Model { get; set; } = "";
+
+        [MaxLength(150)]
+        [Required]
+        public string Brand { get; set; } = "";
+
+        [Required]
+        public float DailyRate { get; set; } = 0;
+    }
+}
---
+++ ArchiLogApi/Data/Models/Reservation.cs
@@ -0,0 +1,26 @@
+﻿using ApiClassLibrary.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArchiLogApi.Data.Models
+{
+    public class Reservation : BaseModel
+    {
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public int TotalCost { get; set; }
+
+        [Required]
+        public int CarID { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CarID))]
+        public virtual Car ReservedCar { get; set; }
+    }
+}
